e6b13aac368ad714e4837e53f8332e7851252c82
Amudajr <classicallow26@gmail.com>
2023-09-29 08:40:02 +0100

    dfgfd

--- lib/pixel_adventure.dart
+++ lib/pixel_adventure.dart
@@ -17,7 +17,10 @@ class PixelAdventure extends FlameGame with HasKeyboardHandlerComponents {
   FutureOr<void> onLoad() async {
     await images.loadAllImages();
     cam = CameraComponent.withFixedResolution(
-        world: world, width: 640, height: 360);
+      world: world,
+      width: 640,
+      height: 360,
+    );
     cam.viewfinder.anchor = Anchor.topLeft;
     addAll([cam, world]);
 
